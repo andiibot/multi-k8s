3cff5e37eaefa727fdaf8af92fadbb3a8f46cf57
Andres <asalonso17@gmail.com>
2021-09-18 15:50:16 -0500

    ver 1.1.1

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Fib Calculator Andy on K8S ver 1.1</h1>
+          <h1 className="App-title">Fib Calculator Andy on K8S ver 1.1.1</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
